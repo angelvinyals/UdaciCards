34140b5c2a5e3f1495955b66dd2985b1aa2d6e90
angelvinyals <angel.vinyals@gmail.com>
2018-08-16 18:34:01 +0200

    add Navigation
    Add five screens
    Add buttons to navigate  (trying v2.0)

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button,View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class DeckListScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Deck List view</Text>
+        <Button
+          title="Go to Individual Deck"
+          onPress={() => this.props.navigation.navigate('IndividualDeck')}
+        />
+        <Button
+          title="Go to Home-(deckList)"
+          onPress={() => this.props.navigation.navigate('DeckList')}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class IndividualDeckScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Individual Deck view</Text>
+        <Button
+          title="Go to Quiz"
+          onPress={() => this.props.navigation.navigate('Quiz')}
+        />
+        <Button
+          title="Go to Home-(deckList)"
+          onPress={() => this.props.navigation.navigate('DeckList')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+class QuizScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Quiz view</Text>
+        <Button
+          title="Go to Home-(deckList)"
+          onPress={() => this.props.navigation.navigate('DeckList')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+class NewDeckScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>New Deck view</Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+class NewQuestionScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>New Question view</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  DeckList: {
+    screen: DeckListScreen,
   },
+  IndividualDeck: {
+    screen: IndividualDeckScreen,
+  },
+  Quiz: {
+    screen: QuizScreen,
+  },
+  NewDeck: {
+    screen: NewDeckScreen,
+  },
+  NewQuestion: {
+    screen: NewQuestionScreen,
+  },
+
 });
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
