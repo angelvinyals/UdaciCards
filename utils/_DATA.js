d57d929fd43fbb248e5d77fc366673df2a81228a
angelvinyals <angel.vinyals@gmail.com>
2018-08-20 18:56:33 +0200

    ASYNCSTORAGE....... Changing decks in DATA
    Because of getim data in every component in order to  be updated after ADD question ,  or  add deck , or.... I change  variable decks converting the array in a object of objects

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -4,38 +4,49 @@
 
 import {AsyncStorage} from 'react-native';
 
-const STORAGE_KEY = 'mobileFlashcards:decks';
+export const STORAGE_KEY = '@mobileFlashcards:decks';
 
-export const decks = [
-    {
-        key: 'decksReact',
+export const decks ={
+    deckReact: {
+        key: 'deckReact',
         title: 'React',
-        questions: [
-            {key:'React1', question: 'What is React?', answer: 'A library for managing user interfaces'},
-            {key:'React2', question: 'Where do you make Ajax requests in React?', answer: 'The componentDidMount lifecycle event'},
-        ]
+        questions:{
+            React1:{key:'React1', question: 'What is React?', answer: 'A library for managing user interfaces'},
+            React2:{key:'React2', question: 'Where do you make Ajax requests in React?', answer: 'The componentDidMount lifecycle event'},
+        },
     },
-    {
-        key: 'decksJavaScript',
+    deckJavaScript: {
+        key: 'deckJavaScript',
         title: 'JavaScript',
-        questions: [
-
-            {key:'JavaScript1', question: 'What is Javascript?', answer: 'A programming language.'},
-            {key:'JavaScript2', question: 'What was Javascript originally named?', answer: 'Mocha'},
-            {
+        questions:{
+            JavaScript1:{key:'JavaScript1', question: 'What is Javascript?', answer: 'A programming language.'},
+            JavaScript2:{key:'JavaScript2', question: 'What was Javascript originally named?', answer: 'Mocha'},
+            JavaScript3:{
                 key:'JavaScript3',
                 question: 'What is a closure?',
                 answer: 'The combination of a function and the lexical environment within which that function was declared.'
-            }
-
-        ]
+            },
+        },
     },
-];mobileFlashcards:decks
+}
 
 
-export const saveDecks = async(decks) => {
+export const saveDecksOld = async(decks) => {
     try{
-        awati AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks));
+        await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks));
+    }catch (error) {
+        // Error retrieving data
+        alert(err);
+    }
+    
+}
+
+export const saveDecks = async(decks_delta) => {
+    try{
+        await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks), () => {
+                        AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify(decks_delta));
+                    });
+        console.log('newDecks:', decks)         
     }catch (error) {
         // Error retrieving data
         alert(err);
@@ -58,17 +69,18 @@ export const fetchDecks = async() => {
     }
 }
 
-export const fetchDeck = async(key) => {
-    try {
-        const data = await AsyncStorage.getItem(STORAGE_KEY);
-        console.log('data = ', data)
-        if (data !== null) {
-            // We have data!!
-            return JSON.parse(data);
-        }
-        return decks;
-    } catch (error) {
+export const saveNewQuestion = async(decks_delta) => {
+    console.log('....inside saveNewQuestion IN DATA.JS')
+    console.log('decks_delta: ',decks_delta)
+    try{
+        await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks2), () => {
+            AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify(decks_delta))
+        });
+        const result = await AsyncStorage.getItem(STORAGE_KEY)
+        console.log('after AsyncStorage....................')
+        console.log('result: ', result)
+    }catch (error) {
         // Error retrieving data
-        alert(err);
+        console.log('saveNewQuestion has launch an error',error);
     }
 }
